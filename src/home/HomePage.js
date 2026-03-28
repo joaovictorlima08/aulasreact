32e750e3e0cbd860b5106a5910a2962d769e7eba
JOAO VICTOR LIMA DOS SANTOS <46123@fiecalunos.lan>
2026-03-27 21:36:59 -0300

    aula dia 27

---
+++ src/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const HomePage = props => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <img src='https://images.ctfassets.net/3mv54pzvptwz/7Jj4ryLGJazS8pDUlCK2Vg/07213977dcbbc3d402bac1e1a27c86af/54331642772_05fa9ffe6b_o_dentro.jpg'/>
+      </div>
+  
+  )
+}
+
+HomePage.propTypes = {
+
+}
+
+export default HomePage
---
+++ src/not-found/NotFoundPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NotFoundPage = props => {
+  return (
+    <div>NotFoundPage</div>
+  )
+}
+
+NotFoundPage.propTypes = {
+
+}
+
+export default NotFoundPage
